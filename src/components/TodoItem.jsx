2ad07d02973f7fbd8030f6947a6e086df761cb2f
arif <arifur.tuc@gmail.com>
2024-02-03 02:45:04 +0100

    Implementing delete button functionality

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,13 +1,22 @@
 import styles from "./todoitem.module.css";
 
-export default function TodoItem({ item }) {
+export default function TodoItem({ item, todos, setTodos }) {
+  // Function to handle the deletion of a to-do item
+  function handleDelete(item) {
+    setTodos(todos.filter((todo) => todo !== item));
+  }
   return (
     <div className={styles.item}>
       <div className={styles.itemName}>
         {item}
         {/* Delete button to remove todos */}
         <span>
-          <button className={styles.deleteButton}>x</button>
+          <button
+            onClick={() => handleDelete(item)}
+            className={styles.deleteButton}
+          >
+            x
+          </button>
         </span>
       </div>
       {/* Horizontal line for visual separation */}
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,12 +1,12 @@
 import TodoItem from "./TodoItem";
 import styles from "./todolist.module.css";
 
-export default function TodoList({ todos }) {
+export default function TodoList({ todos, setTodos }) {
   return (
     <div className={styles.list}>
       {/* Displaying the list of to-do items using TodoItem component */}
       {todos.map((item) => (
-        <TodoItem key={item} item={item} />
+        <TodoItem key={item} item={item} todos={todos} setTodos={setTodos} />
       ))}
     </div>
   );
