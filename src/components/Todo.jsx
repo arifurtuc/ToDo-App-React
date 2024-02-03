7f7495af14a9e5716730460335c7519d0bf1dcaf
arif <arifur.tuc@gmail.com>
2024-02-03 15:32:23 +0100

    Counting and displaying completed, total todos

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,15 +1,18 @@
 import { useState } from "react";
 import Form from "./Form";
 import TodoList from "./TodoList";
+import Footer from "./Footer";
 
 export default function Todo() {
   // State variable to store the list of to-do items
   const [todos, setTodos] = useState([]);
-
+  const completedTodos = todos.filter((todo) => todo.status).length;
+  const totalTodos = todos.length;
   return (
     <div>
       <Form todos={todos} setTodos={setTodos} />
       <TodoList todos={todos} setTodos={setTodos} />
+      <Footer completedTodos={completedTodos} totalTodos={totalTodos} />
     </div>
   );
 }
