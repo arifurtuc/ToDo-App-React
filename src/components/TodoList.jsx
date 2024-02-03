2e12ca4bc067579ca9f4fe3b6a72d68f27b9ad86
arif <arifur.tuc@gmail.com>
2024-02-03 02:25:07 +0100

    Adding CSS to todo items

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,8 +1,9 @@
 import TodoItem from "./TodoItem";
+import styles from "./todolist.module.css";
 
 export default function TodoList({ todos }) {
   return (
-    <div>
+    <div className={styles.list}>
       {/* Displaying the list of to-do items using TodoItem component */}
       {todos.map((item) => (
         <TodoItem key={item} item={item} />
