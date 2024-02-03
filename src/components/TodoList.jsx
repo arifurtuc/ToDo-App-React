1e50049cc30703b36fc45a078d4900af782ce3f7
arif <arifur.tuc@gmail.com>
2024-02-03 15:40:07 +0100

    Sorting todos based on completion status

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -2,10 +2,14 @@ import TodoItem from "./TodoItem";
 import styles from "./todolist.module.css";
 
 export default function TodoList({ todos, setTodos }) {
+  // Sort todos based on completion status (incomplete items first)
+  const sortedTodos = todos
+    .slice()
+    .sort((a, b) => Number(a.status) - Number(b.status));
   return (
     <div className={styles.list}>
       {/* Displaying the list of to-do items using TodoItem component */}
-      {todos.map((item) => (
+      {sortedTodos.map((item) => (
         <TodoItem
           key={item.name}
           item={item}
